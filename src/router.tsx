bfbcb1501670ab5a244186368d01b2770fe84b7d
Noah Weingand <noahweingand@gmail.com>
2026-01-22 17:22:30 -0500

    fix: router ref?

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,41 @@
 import { createRouter as createReactRouter } from "@tanstack/react-router";
 
 import { routeTree } from "./routeTree.gen";
+import { useRef } from "react";
 
 export function createRouter() {
-    return createReactRouter({
+    // TODO: fix lazy typing
+    const ref: { current: any } = { current: null };
+
+    const router = createReactRouter({
         routeTree,
         context: {
             assets: { js: [], css: [] },
+            requestId: ''
         },
         // defaultPreload: false,
         scrollRestoration: true,
         notFoundMode: "root",
+        dehydrate: () => {
+            console.log("router dehydrated", ref.current);
+            return {
+                requestId: ref.current.options.context.requestId
+            }
+        },
+        hydrate: (dehydrated) => {
+            console.log("dehydrated data on hydrate", dehydrated)
+            ref.current.update({
+                context: {
+                    ...ref.current.options.context,
+                    requestId: dehydrated.requestId
+                }
+            })
+        }
     });
+
+    ref.current = router;
+
+    return router;
 }
 
 declare module "@tanstack/react-router" {
